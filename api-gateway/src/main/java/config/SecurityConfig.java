6a8837825b8de1f34b398ed2b65c57f6b2fdcef0
Hüseyin Türkmen <huseyinturkmen2882@gmail.com>
2023-03-13 21:14:18 +0300

    api-gateway-added

---
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+//we are using webflux securtiy instead of webmvc security
+//because our app linked to webflux
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf().disable()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+
+
+
+}
